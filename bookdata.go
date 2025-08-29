bd198ba31d8eabe130a0bc35b71c78e0abf6590b
John Martincic <john@martincicjohn.com>
2025-08-29 13:21:08 -0500

    feat: Initial Upload
    - Upload of initially created go.mod
    - Upload of provided bookdata.go

---
+++ bookdata.go
@@ -0,0 +1,27 @@
+package main
+
+var books = []string {
+	"The Tale of Two Cities",
+	"The Joy of Compassion",
+	"From Earth to Sirius",
+	"Galaxy of Three Pebbles",
+	"House of Impatience",
+	"Raffle Got the Blues",
+	"Where the Rainbows Have No Ends",
+	"Storm, Typhoons, Snow, and Sunshine",
+	"You Can't Say That Ever Again",
+	"Tally of the Seashells",
+	"For Whom the Snails Toast",
+	"Where Did All the Greek Gods Go",
+	"The Elemental Drift",
+	"Earthers Beware",
+	"Welcome to Our Giant House on the Hudson",
+	"I Have Something Not Nice to Say",
+	"The Dinner Table of Awkwardness",
+	"Sunshine is Overrated",
+	"The Planet of Green Cities",
+	"Remembering the Yellow Train Tracks",
+	"Seasick on the Land",
+	"Blue Infini",
+	"Blue Planet Red Ambition",
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module goread
+
+go 1.25.0
